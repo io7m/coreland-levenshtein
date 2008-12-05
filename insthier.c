a95008d8282dc773a7e7c42941f67beea64fdd34
m0 <m0@localhost>
2008-12-05 06:13:14 +0000

    regenerate insthier

--- insthier.c
+++ insthier.c
@@ -22,5 +22,7 @@ struct install_item insthier[] = {
   {INST_COPY, "levenshtein.a", "liblevenshtein.a", ctxt_slibdir, 0, 0, 0644},
   {INST_COPY, "levenshtein-conf.ld", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY_EXEC, "levenshtein-conf", 0, ctxt_bindir, 0, 0, 0755},
+  {INST_COPY, "levenshtein-comp.ald", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY_EXEC, "levenshtein-comp", 0, ctxt_bindir, 0, 0, 0755},
 };
 unsigned long insthier_len = sizeof(insthier) / sizeof(struct install_item);
